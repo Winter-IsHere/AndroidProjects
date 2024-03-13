131c16109f5697f7ffba42393d520ab4ca60137e
shivami <shivami@kpit.com>
2024-03-13 15:09:30 +0530

    Setting up MainViewModel and fetching data and preparing the recipeScreen

---
+++ MyRecipeApp/app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
@@ -0,0 +1,34 @@
+package com.example.myrecipeapp
+
+import android.graphics.Paint.Align
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun RecipeScreen(modifier: Modifier = Modifier){
+    val recipeViewModel : MainViewModel = viewModel()
+    val viewstate by recipeViewModel.categoriesState
+    Box(modifier = Modifier.fillMaxSize()){
+        when{
+            viewstate.loading ->{
+                CircularProgressIndicator(modifier.align(Alignment.Center))
+            }
+
+            viewstate.error != null ->{
+                Text(text = "ERROR OCCURRED")
+            }
+            else ->{
+                //Display categories
+
+            }
+        }
+    }
+}
