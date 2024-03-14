5a2b8ec40367a93658dd04336a768d2a1fe00cf5
shivami <shivami@kpit.com>
2024-03-14 12:28:39 +0530

    Created a basic calculator app for performing basic mathematical operations using XML

--- FirstXMLApplication/app/src/main/java/com/example/firstxmlapplication/MainActivity.kt
+++ FirstXMLApplication/app/src/main/java/com/example/firstxmlapplication/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.firstxmlapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMultiply = findViewById<Button>(R.id.buttonMultiply)
+        val btnSubtract = findViewById<Button>(R.id.buttonSub)
+        val btnAdd = findViewById<Button>(R.id.buttonAdd)
+        val btnDivide = findViewById<Button>(R.id.buttonDivide)
+        val answer = findViewById<TextView>(R.id.textView)
+        val num1  = findViewById<TextInputEditText>(R.id.textInputEditText)
+        val num2 = findViewById<TextInputEditText>(R.id.textInputEditText2)
+
+        btnMultiply.setOnClickListener{
+            val input1 = num1.text.toString().toDouble()
+            val input2 = num2.text.toString().toDouble()
+            val result = input1 * input2
+            answer.text = result.toString()
+        }
+        btnSubtract.setOnClickListener{
+            val input1 = num1.text.toString().toDouble()
+            val input2 = num2.text.toString().toDouble()
+            val result = input1 - input2
+            answer.text = result.toString()
+        }
+        btnAdd.setOnClickListener{
+            val input1 = num1.text.toString().toDouble()
+            val input2 = num2.text.toString().toDouble()
+            val result = input1 + input2
+            answer.text = result.toString()
+        }
+        btnDivide.setOnClickListener{
+            val input1 = num1.text.toString().toDouble()
+            val input2 = num2.text.toString().toDouble()
+            if (input2 == 0.0){
+                answer.text = "Cannot divide by 0"
+            }
+            else{
+                val result = input1 / input2
+                answer.text = result.toString()
+            }
+
+        }
     }
 }
