98ec6e452e09ee068d4c41b5a5d66a1e3a9f2c18
shivami <shivami@kpit.com>
2024-03-20 23:18:37 +0530

    Added a new project AgeInMinutesCalculator

--- AgeInMinutesCalculator/app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
+++ AgeInMinutesCalculator/app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.ageinminutescalculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var myTextView = findViewById<TextView>(R.id.myText)
+        myTextView.text = "Here Comes the Pain"
     }
 }
---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MoreFundamentals.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinbasics
+
+
+class OuterClass{
+    private val name: String = "Hello"
+    val num : Int = 2
+    inner class InnerClass{
+        var description : String = "This is code which is inside nested class"
+        private val id : Int = 101
+        fun foo(){
+            println("Name is $name") //Cannot access the outer class member
+            println("ID is $id")
+        }
+    }
+}
+fun main(){
+    //nested class must be initialized
+    println(OuterClass().InnerClass().description)
+
+    var obj1 = OuterClass()
+    obj1.InnerClass().foo()
+}
