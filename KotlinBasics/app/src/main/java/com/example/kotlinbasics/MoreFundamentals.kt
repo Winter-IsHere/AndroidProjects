763dbd9c424cedeb82efdeaff7fb3787fe65b386
shivami <shivami@kpit.com>
2024-03-22 16:24:20 +0530

    Practiced some kotlin concepts

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/MoreFundamentals.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/MoreFundamentals.kt
@@ -3,7 +3,7 @@ package com.example.kotlinbasics
 
 class OuterClass{
     private val name: String = "Hello"
-    val num : Int = 2
+//    val num : Int = 2
     inner class InnerClass{
         var description : String = "This is code which is inside nested class"
         private val id : Int = 101
@@ -17,6 +17,6 @@ fun main(){
     //nested class must be initialized
     println(OuterClass().InnerClass().description)
 
-    var obj1 = OuterClass()
+    val obj1 = OuterClass()
     obj1.InnerClass().foo()
 }
