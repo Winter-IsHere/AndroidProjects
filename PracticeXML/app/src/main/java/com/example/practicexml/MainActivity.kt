32f8c6c0fb82ef70a629fc8b559aa5c9225153ce
shivami <shivami@kpit.com>
2024-03-27 23:26:11 +0530

    Added a new branch to practice toast messages

--- PracticeXML/app/src/main/java/com/example/practicexml/MainActivity.kt
+++ PracticeXML/app/src/main/java/com/example/practicexml/MainActivity.kt
@@ -11,24 +11,7 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.checkboxandradio)
-        var radioMeatGroup = findViewById<RadioGroup>(R.id.radioMeatGroup)
-        var cbOnion = findViewById<CheckBox>(R.id.cbOnion)
-        var cbCheese = findViewById<CheckBox>(R.id.cbCheese)
-        var cbSalad = findViewById<CheckBox>(R.id.cbSalad)
-        var result = findViewById<TextView>(R.id.tvOrderResult)
-        var btnOrder = findViewById<Button>(R.id.btnOrder)
+        setContentView(R.layout.toast)
 
-        btnOrder.setOnClickListener{
-            val checkedRadioMeatButtonId = radioMeatGroup.checkedRadioButtonId
-            val meat = findViewById<RadioButton>(checkedRadioMeatButtonId)
-            val cheese = cbCheese.isChecked
-            val onion = cbOnion.isChecked
-            val salad = cbSalad.isChecked
-            val orderString = "You ordered a burger with:\n${meat.text}" + (if (cheese) "\nCheese" else "") +
-                    (if (onion) "\nOnion" else "") +
-                    (if (salad) "\nSalad" else "")
-            result.text = orderString
-        }
     }
 }
