63886e53ac472260f014396f56851effeb1be201
shivami <shivami@kpit.com>
2024-03-27 23:23:28 +0530

    Practicing differnt XML concepts

--- PracticeXML/app/src/main/java/com/example/practicexml/MainActivity.kt
+++ PracticeXML/app/src/main/java/com/example/practicexml/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.practicexml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.checkboxandradio)
+        var radioMeatGroup = findViewById<RadioGroup>(R.id.radioMeatGroup)
+        var cbOnion = findViewById<CheckBox>(R.id.cbOnion)
+        var cbCheese = findViewById<CheckBox>(R.id.cbCheese)
+        var cbSalad = findViewById<CheckBox>(R.id.cbSalad)
+        var result = findViewById<TextView>(R.id.tvOrderResult)
+        var btnOrder = findViewById<Button>(R.id.btnOrder)
+
+        btnOrder.setOnClickListener{
+            val checkedRadioMeatButtonId = radioMeatGroup.checkedRadioButtonId
+            val meat = findViewById<RadioButton>(checkedRadioMeatButtonId)
+            val cheese = cbCheese.isChecked
+            val onion = cbOnion.isChecked
+            val salad = cbSalad.isChecked
+            val orderString = "You ordered a burger with:\n${meat.text}" + (if (cheese) "\nCheese" else "") +
+                    (if (onion) "\nOnion" else "") +
+                    (if (salad) "\nSalad" else "")
+            result.text = orderString
+        }
     }
 }
