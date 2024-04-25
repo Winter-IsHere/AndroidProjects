f9263006b7ea2c79d2f52db43390a9a5cb6dbad5
shivami <shivami@kpit.com>
2024-04-25 10:46:40 +0530

    Set up ROOM DB along with kotlin Coroutine

--- RooomDB/app/build.gradle.kts
+++ RooomDB/app/build.gradle.kts
@@ -46,8 +46,17 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("androidx.room:room-runtime:2.5.1")
-    annotationProcessor("androidx.room:room-compiler:2.5.1")
-    kapt("androidx.room:room-compiler:2.3.0")
+
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$room_version")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1-Beta")
 
 }
--- RooomDB/app/src/main/java/com/example/rooomdb/MainActivity.kt
+++ RooomDB/app/src/main/java/com/example/rooomdb/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.rooomdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: ContactDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvDisplayData = findViewById<TextView>(R.id.tvDisplayData)
+
+        database = Room.databaseBuilder(
+            applicationContext,
+            ContactDatabase::class.java,
+            "ContactDB").build()
+
+        GlobalScope.launch {
+            database.contactDao().insertContact(Contact(0,"Winter","007"))
+        }
+
+        tvDisplayData.setOnClickListener {
+            database.contactDao().getContact().observe(this, Observer {
+                tvDisplayData.text = it.toString()
+            })
+        }
     }
 }
